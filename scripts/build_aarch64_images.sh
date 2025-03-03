2f6e1ac2c4450f4589d189fca350d29a733cef2b
NathanFreeman <1056159381@qq.com>
2025-03-04 00:50:39 +0800

    add aarch64 ci test

--- scripts/build_aarch64_images.sh
+++ scripts/build_aarch64_images.sh
@@ -11,16 +11,3 @@ cd ./dockerfiles/tinyproxy && docker build . && cd -
 
 git clone https://github.com/postmanlabs/httpbin.git
 cd ./httpbin && docker build . -t kennethreitz/httpbin && cd -
-
-mkdir brook-src && cd brook-src && wget https://github.com/txthinking/brook/releases/download/v20250202/brook_linux_arm5
-mv brook_linux_arm5 brook
-chmod 0755 brook
-cat <<EOF >Dockerfile
-FROM ubuntu:latest
-
-RUN mkdir /brook-src
-COPY brook /brook-src/brook
-EXPOSE 1080
-CMD ["/brook-src/brook", "socks5", "--listen", "0.0.0.0:1080"]
-EOF
-docker build . -t swoole/brook
