3dfdfdc0d7d9f92c38f1f423de28a63bdcf15cff
NathanFreeman <1056159381@qq.com>
2025-03-22 20:39:09 +0800

    add aarch64 ci test

--- scripts/library.sh
+++ scripts/library.sh
@@ -1,11 +1,18 @@
 #!/bin/sh -e
 apt update
 apt search odbc-mariadb
-apt install -y libaio-dev libaio1 sqlite3 libsqlite3-dev unixodbc unixodbc-dev libzstd-dev
-wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linuxx64.zip
-unzip instantclient-basiclite-linuxx64.zip && rm instantclient-basiclite-linuxx64.zip
-wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-sdk-linuxx64.zip
-unzip instantclient-sdk-linuxx64.zip && rm instantclient-sdk-linuxx64.zip
+apt install -y libaio-dev sqlite3 libsqlite3-dev unixodbc unixodbc-dev libzstd-dev
+
+if [ "`uname -m`" = "aarch64" ]; then
+  arch="-arm64"
+else
+  arch="x64"
+fi
+
+wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linux${arch}.zip
+unzip instantclient-basiclite-linux${arch}.zip && rm instantclient-basiclite-linux${arch}.zip
+wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-sdk-linux${arch}.zip
+unzip instantclient-sdk-linux${arch}.zip && rm instantclient-sdk-linux${arch}.zip
 mv instantclient_*_* ./instantclient
 rm ./instantclient/sdk/include/ldap.h
 # fix debug build warning: zend_signal: handler was replaced for signal (2) after startup
