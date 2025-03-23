42efa8abc2834403f62ba247c78acb5bf42a1473
NathanFreeman <1056159381@qq.com>
2025-03-23 12:17:10 +0800

    add aarch64 ci test

--- scripts/library.sh
+++ scripts/library.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
+apt update -y
 apt search odbc-mariadb*
-apt update -y && apt upgrade -y
 apt install -y libaio-dev libaio1 sqlite3 libsqlite3-dev unixodbc unixodbc-dev odbc-mariadb libzstd-dev
 
 if [ "`uname -m`" = "aarch64" ]; then
