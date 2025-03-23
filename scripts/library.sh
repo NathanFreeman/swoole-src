5a0a62e7f5a9c1245c2940d0a19b0e6ce56efd76
NathanFreeman <1056159381@qq.com>
2025-03-23 13:00:23 +0800

    add aarch64 ci test

--- scripts/library.sh
+++ scripts/library.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -e
 apt update -y
 apt search odbc*
-apt install -y libaio-dev libaio1 sqlite3 libsqlite3-dev unixodbc unixodbc-dev odbc-mariadb libzstd-dev
+apt install -y libaio-dev libaio1 sqlite3 libsqlite3-dev unixodbc unixodbc-dev libzstd-dev
 
 if [ "`uname -m`" = "aarch64" ]; then
   arch="-arm64"
+  apt install odbc-mdbtools
 else
   arch="x64"
+  apt install odbc-mariadb
 fi
 
 wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linux${arch}.zip
