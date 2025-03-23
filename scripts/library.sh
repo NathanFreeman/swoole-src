98fc4060d05e7a7dc0e8411f7e69b40ddca40377
NathanFreeman <1056159381@qq.com>
2025-03-23 14:16:13 +0800

    add aarch64 ci test

--- scripts/library.sh
+++ scripts/library.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -e
-tee /etc/apt/sources.list.d/ubuntu.sources > /dev/null <<EOL
-Types: deb
-URIs: https://mirrors.aliyun.com/ubuntu/
-Suites: noble noble-updates noble-backports noble-security
-Components: main restricted universe multiverse
-Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
-EOL
-
 apt update -y
 apt install -y libaio-dev libaio1 sqlite3 libsqlite3-dev unixodbc unixodbc-dev odbc-mariadb libzstd-dev
 
