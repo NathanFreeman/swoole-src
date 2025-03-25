21189c3a46a8c81f58f0f6ff38d86509b87632c0
NathanFreeman <1056159381@qq.com>
2025-03-26 00:10:58 +0800

    add aarch64 ci test

--- scripts/library.sh
+++ scripts/library.sh
@@ -1,26 +1,28 @@
 #!/bin/sh -e
 apt update && apt install -y gnupg2
-tee /etc/apt/sources.list >/dev/null <<EOL
-deb https://mirrors.cloud.tencent.com/ubuntu-ports/ noble main restricted universe multiverse
-deb-src https://mirrors.cloud.tencent.com/ubuntu-ports/ noble main restricted universe multiverse
-deb https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-security main restricted universe multiverse
-deb-src https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-security main restricted universe multiverse
-deb https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-updates main restricted universe multiverse
-deb-src https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-updates main restricted universe multiverse
-deb https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-backports main restricted universe multiverse
-deb-src https://mirrors.cloud.tencent.com/ubuntu-ports/ noble-backports main restricted universe multiverse
-EOL
-
 apt-key adv --keyserver keyserver.ubuntu.com --recv 871920D1991BC93C
-apt update
-apt install -y unixodbc-dev libaio-dev libaio1 sqlite3 libsqlite3-dev libzstd-dev odbc-maraidb
 
 if [ "$(uname -m)" = "aarch64" ]; then
   arch="-arm64"
+  mirror="https://mirrors.cloud.tencent.com/ubuntu-ports/"
 else
   arch="x64"
+  mirror="https://mirrors.cloud.tencent.com/ubuntu/"
 fi
 
+tee /etc/apt/sources.list >/dev/null <<EOL
+deb ${mirror} noble main restricted universe multiverse
+deb-src ${mirror} noble main restricted universe multiverse
+deb ${mirror} noble-security main restricted universe multiverse
+deb-src ${mirror} noble-security main restricted universe multiverse
+deb ${mirror} noble-updates main restricted universe multiverse
+deb-src ${mirror} noble-updates main restricted universe multiverse
+deb ${mirror} noble-backports main restricted universe multiverse
+deb-src ${mirror} noble-backports main restricted universe multiverse
+EOL
+
+apt update
+apt install -y unixodbc-dev libaio-dev libaio1 sqlite3 libsqlite3-dev libzstd-dev odbc-maraidb
 wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linux${arch}.zip
 unzip instantclient-basiclite-linux${arch}.zip && rm instantclient-basiclite-linux${arch}.zip
 wget -nv https://download.oracle.com/otn_software/linux/instantclient/instantclient-sdk-linux${arch}.zip
